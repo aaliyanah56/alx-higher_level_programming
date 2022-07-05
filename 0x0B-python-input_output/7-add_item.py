12294332e5a5c13c18a36929b7341c03eb27def8
aaliyanah56 <rahmaadannoor@gmail.com>
2022-07-05 14:07:25 +0300

    0x0B-python_input

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""module to load and save"""
+"""
+a script that adds all arguments
+"""
 import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+args = list(sys.argv[1:])
+
+try:
+    args_data = load_from_json_file("add_item.json")
+
+except Exception:
+    args_data = []
+
+args_data.extend(args)
+save_to_json_file(args_data, "add_item.json")
